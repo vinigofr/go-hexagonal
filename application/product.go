2e0d253d14225ffc8ed5c55c077b1f55d6ccd372
Vinicius Gouveia <freitas.viniciuspk@outlook.com.br>
2022-11-06 20:24:24 -0300

    Create product.go with interface, struct e methods

---
+++ application/product.go
@@ -0,0 +1,60 @@
+package application
+
+import "errors"
+
+// Uma interface carregará métodos
+type ProductInterface interface {
+	IsValid() (bool, error)
+	Enable() error
+	Disable() error
+	GetId() string
+	GetName() string
+	GetStatus() string
+	GetPrice() float64
+}
+
+const (
+	DISABLED = "disabled"
+	ENABLED  = "enabled"
+)
+
+// Estrutura, por exemplo, do banco
+type Product struct {
+	ID     string
+	Name   string
+	Price  float64
+	Status string
+}
+
+func (p *Product) IsValid() (bool, error) {
+	return true, nil
+}
+
+func (p *Product) Enable() error {
+	if p.Price > 0 {
+		p.Status = ENABLED
+		return nil
+	}
+
+	return errors.New("the price must be greather than zero to enable the product")
+}
+
+func (p *Product) Disable() error {
+	return nil
+}
+
+func (p *Product) GetId() string {
+	return p.ID
+}
+
+func (p *Product) GetName() string {
+	return p.Name
+}
+
+func (p *Product) GetStatus() string {
+	return p.Status
+}
+
+func (p *Product) GetPrice() float64 {
+	return p.Price
+}
